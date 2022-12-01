14ef299484de7c2da5e20dbf5e13d75e626f258d
strawberrybinie <breadsoobinie.0512@gmail.com>
2022-11-30 20:26:45 -0600

    Pseudo codigo de la parte 2 del programa y menu

--- Ciclos_CYP04.cpp
+++ Ciclos_CYP04.cpp
@@ -1,71 +1,26 @@
 /*Crear un programa que dado un intervalo y un incremento calcule los valores del numero 'e'
   Tomando en cuenta que el programa será llamado por un bot y no por un usuario
-  Por lo que no requiere de interfaz*/
+  Por lo que no requiere de interfaz
+  Ahora se agregara un programa que calcule el encabezado simple de un archivo BPM y con ello
+  un menu de opciones*/
 
+//se agregan la(s) biblioteca(s)
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-	double limInicio;
-	double limFinal;
-	double incremento;
+	//se imprime en pantalla el menu de opciones con los incisos a elegir luego un scanf que lo lea
 
-	double resulExp;
-	double resulE = 0;
 
-	int j;
-	int z;
-	int fact;
+	/*se agrega un if con la condición de si se introduce el inciso 1, llevar al programa señalado
+	  o el 2, llevar al programa señalado, de no ser así, mostrar un mensaje de error*/
 
-<<<<<<< Updated upstream
-	//Haremos uso de un scanf para leer los valores que se van a ingresar
-=======
->>>>>>> Stashed changes
-	scanf_s("%lf", &limInicio);
-	scanf_s("%lf", &limFinal);
-	scanf_s("%lf", &incremento);
-	
-	int conteo = (limFinal - limInicio) / incremento;
-<<<<<<< Updated upstream
-	/*La condición del while será que mientras el limite inicial sea menor o igual a la suma 
-	  del limite final con el incremento entre*/
-=======
+	/*cuando la condicion nos lleva al programa del bpm, declaramos los valores necesarios,
+	  el tamaño, los dos reservados, el offbits, y los tamaños en hexadecimales*/
 
-	if (limInicio == 0 && limFinal == 0)
-	{
-		resulExp = 1;
-		resulE = 1;
-		printf("%f %f %f", limInicio, resulE, resulExp);
-		return 0;
-	}
-	else if (incremento == 0)
-	{
-		return 1;
-	}
->>>>>>> Stashed changes
-	while (limInicio <= limFinal + incremento)
-	{
-		for (j = 0; j <= conteo; j++)
-		{
-			fact = 1;
-			for (z = 1; z <= j; z++)
-			{
-				fact = fact * z;
-			}
-			resulE = resulE + (pow(limInicio, j)) / fact;
-		}
-		resulExp = exp(limInicio);
-<<<<<<< Updated upstream
-		/*Por ultimo imprimimos los datos en pantalla, agregamos el incremento al limite incial para que
-	      vuelva al while y se repita, de no ser así que regrese a 0*/
-		printf("%f %f %f\n", limInicio, resulE, resulExp);
-=======
+	//escaneamos los valores del tamaño y los bits para inicializar los otros en 0
 
-		printf("Calculo LI: %f \tCalculo 'E': %f \tCalculo Exp: %f\n", limInicio, resulE, resulExp);
->>>>>>> Stashed changes
-		limInicio = limInicio + incremento;
-		resulE = 0;
-	}
+	//Hacemos la operación que transforme nuestros numeros y los imprimimos en pantalla
 
+	//aquí acaba el programa
 }
