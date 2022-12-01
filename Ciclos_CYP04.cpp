579404bef5e91fad6aa3d67fa57d7981647745e8
strawberrybinie <breadsoobinie.0512@gmail.com>
2022-11-30 20:45:01 -0600

    Prueba del programa BPM con menu e interfaz

--- Ciclos_CYP04.cpp
+++ Ciclos_CYP04.cpp
@@ -5,6 +5,7 @@
 
   //Incluimos las bibliotecas necesarias, debido a que usaremos funciones matematicas agregamos math
 #include <stdio.h>
+<<<<<<< Updated upstream
 #include <math.h>
 
 int main()
@@ -24,6 +25,47 @@ int main()
 	int j;
 	int z;
 	int fact;
+=======
+#include <string.h>
+
+int main()
+{
+	//se imprime en pantalla el menu de opciones con los incisos a elegir luego un scanf que lo lea
+	int opc=0;
+	printf("\t\t\tCalcula... Programa\nQue desea hacer?\n");
+	printf("1. Calcular 'E'\n2. Calcular encabezado BPM\n");
+	printf("\tTeclee el numero de la opcion que desea: ");
+	scanf_s("%i", &opc);
+
+	/*se agrega un if con la condición de si se introduce el inciso 1, llevar al programa señalado
+	  o el 2, llevar al programa señalado, de no ser así, mostrar un mensaje de error*/
+	if (opc == 1)
+	{
+		//Al ser una prueba, el programa que calcula 'e' aún no será agregado
+		printf("calculo e");
+	}
+	else if (opc == 2)
+	{
+		/*cuando la condicion nos lleva al programa del bpm, declaramos los valores necesarios,
+	  el tamaño, los dos reservados, el offbits, y los tamaños en hexadecimales*/
+		int bfSize, bfReserved1, bfReserved2, bfOffBits, hexaSize, hexaBits;
+
+		//haremos uso de interfaz
+		printf("Medida: ");
+		//escaneamos los valores del tamaño y los bits para inicializar los otros en 0
+		scanf_s("%i", &bfSize);
+		bfReserved1 = 0000;
+		bfReserved2 = 0000;
+		printf("OffBits: ");
+		scanf_s("%i", &bfOffBits);
+		//Hacemos la operación que transforme nuestros numeros y los imprimimos en pantalla
+		hexaSize = bfSize;
+		hexaBits = bfOffBits;
+
+		printf("%c%c%08X%04i%04i%08X", 66, 77, hexaSize, bfReserved1, bfReserved2, hexaBits);
+	}
+	else printf("Valor no valido");
+>>>>>>> Stashed changes
 
 	//Haremos uso de un scanf para leer los valores que se van a ingresar
 	printf("\t\tCalcula el valor de 'e'\nIntroduce tu limite inicial: ");
